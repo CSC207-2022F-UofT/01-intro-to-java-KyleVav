31a65bb5ec5c98de14dcf12190d8df9b61c51b15
KyleVav <114881362+KyleVav@users.noreply.github.com>
2022-10-02 16:53:01 -0400

    Update Basics.java

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -151,7 +151,7 @@ public class Basics {
         // Fill in the rest of the body here
         String[] split_str = to_split.split(" ");
         for (int i = 0; i < split_str.length; i++) {
-            ret.append(split_str.charAt(i));
+            ret = ret + split_str.charAt(i);
         }
 
         return ret.toString();
